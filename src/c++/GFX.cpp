3e4bdb37c67f3a891338c55ba6a113c0adee0568
Anthony Castiglia <castiglia.anthony@gmail.com>
2013-09-29 12:03:38 -0400

    Changed from tabbed to 2-space indentation

--- src/c++/GFX.cpp
+++ src/c++/GFX.cpp
@@ -8,141 +8,141 @@
 #include "GFX.h"
 
 GFXWindow::GFXWindow(int h, int w) : height(h), width(w), vshaderPath(0),
-	fshaderPath(0) {
-	std::vector<std::vector<GLfloat> > objects;
-}
+  fshaderPath(0) {
+    std::vector<std::vector<GLfloat> > objects;
+  }
 
 void GFXWindow::setBG(float r, float g, float b, float a) {
-	this->r = r;
-	this->g = g;
-	this->b = b;
-	this->a = a;
-	glClearColor(r, g, b, a);
+  this->r = r;
+  this->g = g;
+  this->b = b;
+  this->a = a;
+  glClearColor(r, g, b, a);
 }
 
 void GFXWindow::setCamera(glm::vec3 eyepoint, glm::vec3 lookat, glm::vec3 up) {
-	this->eyepoint = eyepoint;
-	this->lookat = lookat;
-	this->up = up;
+  this->eyepoint = eyepoint;
+  this->lookat = lookat;
+  this->up = up;
 }
 
 void GFXWindow::setVertexShader(const char* path) {
-	vshaderPath = path;
+  vshaderPath = path;
 }
 
 void GFXWindow::setFragmentShader(const char* path) {
-	fshaderPath = path;
+  fshaderPath = path;
 }
 
 void GFXWindow::addObject(GFXObject obj) {
-	objects.push_back(obj);
+  objects.push_back(obj);
 }
 
 GFXObject GFXWindow::getObject(int ind) {
-	return objects[ind];
+  return objects[ind];
 }
 
 void GFXWindow::createWindow() {
-	if (!glfwInit()) {
-		log(ERROR, "Failed to initialize GLFW");
-		quit(-1);
-	}
+  if (!glfwInit()) {
+    log(ERROR, "Failed to initialize GLFW");
+    quit(-1);
+  }
 
-	if (!glfwOpenWindow(width, height, 
-				COLOR_DEPTH, COLOR_DEPTH, COLOR_DEPTH, ALPHA_DEPTH, 
-				DEPTH_BUFFER_BITS, STENCIL_BUFFER_BITS, GLFW_WINDOW)) {
-		log(ERROR, "Failed to create GLFW window");
-		quit(-1);
-	}
+  if (!glfwOpenWindow(width, height, 
+        COLOR_DEPTH, COLOR_DEPTH, COLOR_DEPTH, ALPHA_DEPTH, 
+        DEPTH_BUFFER_BITS, STENCIL_BUFFER_BITS, GLFW_WINDOW)) {
+    log(ERROR, "Failed to create GLFW window");
+    quit(-1);
+  }
 }
 
 void GFXWindow::init() {
-	// Check for an open window w/ rendering context, create one
-	// if there isn't one already open
-	if (!glfwGetWindowParam(GLFW_OPENED)) {
-		createWindow();
-	}
+  // Check for an open window w/ rendering context, create one
+  // if there isn't one already open
+  if (!glfwGetWindowParam(GLFW_OPENED)) {
+    createWindow();
+  }
 
-	glewExperimental = GL_TRUE;
-	if (glewInit() != GLEW_OK) {
-		log(ERROR, "Failed to initialize GLEW");
-		quit(-1);
-	}
-	
-	glGenVertexArrays(1, &vao);
-	glBindVertexArray(vao);
+  glewExperimental = GL_TRUE;
+  if (glewInit() != GLEW_OK) {
+    log(ERROR, "Failed to initialize GLEW");
+    quit(-1);
+  }
 
-	// std::vector<GLfloat> vertex_data = getObject(0);
-	GFXObject obj = getObject(0);
-	std::vector<GLfloat> vertex_data = obj.first;
-	std::vector<GLushort> faces_data= obj.second;
+  glGenVertexArrays(1, &vao);
+  glBindVertexArray(vao);
 
-	// Create and bind the vertex buffer
-	GLuint vbo;
-	glGenBuffers(1, &vbo);
-	glBindBuffer(GL_ARRAY_BUFFER, vbo);
-	glBufferData(
-			GL_ARRAY_BUFFER, 
-			vertex_data.size() * sizeof(GLfloat), 
-			&vertex_data[0], 
-			GL_STATIC_DRAW);	
+  // std::vector<GLfloat> vertex_data = getObject(0);
+  GFXObject obj = getObject(0);
+  std::vector<GLfloat> vertex_data = obj.first;
+  std::vector<GLushort> faces_data= obj.second;
 
-	// Create and bind the element (index) buffer
-	GLuint ibo;
-	glGenBuffers(1, &ibo);
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
-	glBufferData(
-			GL_ELEMENT_ARRAY_BUFFER, 
-			faces_data.size() * sizeof(GLushort),
-			&faces_data[0],
-			GL_STATIC_DRAW);
+  // Create and bind the vertex buffer
+  GLuint vbo;
+  glGenBuffers(1, &vbo);
+  glBindBuffer(GL_ARRAY_BUFFER, vbo);
+  glBufferData(
+      GL_ARRAY_BUFFER, 
+      vertex_data.size() * sizeof(GLfloat), 
+      &vertex_data[0], 
+      GL_STATIC_DRAW);	
 
-	GLuint shaderProgram;
+  // Create and bind the element (index) buffer
+  GLuint ibo;
+  glGenBuffers(1, &ibo);
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+  glBufferData(
+      GL_ELEMENT_ARRAY_BUFFER, 
+      faces_data.size() * sizeof(GLushort),
+      &faces_data[0],
+      GL_STATIC_DRAW);
 
-	try {
-		shaderProgram = shader_program_from_files(vshaderPath, fshaderPath);
-	} catch (const char* err) {
-		log(ERROR, err);
-		quit(-1);
-	}
+  GLuint shaderProgram;
 
-	uniform_mvp = glGetUniformLocation(shaderProgram, "mvp");
+  try {
+    shaderProgram = shader_program_from_files(vshaderPath, fshaderPath);
+  } catch (const char* err) {
+    log(ERROR, err);
+    quit(-1);
+  }
 
-	if (uniform_mvp == -1) {
-		log(ERROR, "Could not bind uniform variable 'mvp' in vertex shader");
-		quit(-1);
-	}
+  uniform_mvp = glGetUniformLocation(shaderProgram, "mvp");
 
-	GLint position_attribute = glGetAttribLocation(shaderProgram, "position");
-	glVertexAttribPointer(position_attribute, VERTEX_SIZE, GL_FLOAT, GL_FALSE, 0, 0);
+  if (uniform_mvp == -1) {
+    log(ERROR, "Could not bind uniform variable 'mvp' in vertex shader");
+    quit(-1);
+  }
 
-	// Enable the attribute
-	glEnableVertexAttribArray(position_attribute);
+  GLint position_attribute = glGetAttribLocation(shaderProgram, "position");
+  glVertexAttribPointer(position_attribute, VERTEX_SIZE, GL_FLOAT, GL_FALSE, 0, 0);
+
+  // Enable the attribute
+  glEnableVertexAttribArray(position_attribute);
 }
 
 void GFXWindow::idle(glm::mat4 model_transform) {
-	glm::mat4 view = glm::lookAt(eyepoint, lookat, up);
-	glm::mat4 projection = glm::perspective(45.0f, 1.0f * width / height, 0.1f,
-			10.0f);
-	mvp = projection * view * model_transform;
-	glUniformMatrix4fv(uniform_mvp, 1, GL_FALSE, glm::value_ptr(mvp));
+  glm::mat4 view = glm::lookAt(eyepoint, lookat, up);
+  glm::mat4 projection = glm::perspective(45.0f, 1.0f * width / height, 0.1f,
+      10.0f);
+  mvp = projection * view * model_transform;
+  glUniformMatrix4fv(uniform_mvp, 1, GL_FALSE, glm::value_ptr(mvp));
 }
 
 void GFXWindow::render() {
-	int size;
+  int size;
 
-	glEnable(GL_DEPTH_TEST);
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	glBindVertexArray(vao);
-	glGetBufferParameteriv(GL_ELEMENT_ARRAY_BUFFER, GL_BUFFER_SIZE, &size);
-	glDrawElements(GL_TRIANGLES, size / sizeof(GLushort), GL_UNSIGNED_SHORT, 0);	
-	glFlush();
-	
-	glfwSwapBuffers();
+  glEnable(GL_DEPTH_TEST);
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+  glBindVertexArray(vao);
+  glGetBufferParameteriv(GL_ELEMENT_ARRAY_BUFFER, GL_BUFFER_SIZE, &size);
+  glDrawElements(GL_TRIANGLES, size / sizeof(GLushort), GL_UNSIGNED_SHORT, 0);	
+  glFlush();
+
+  glfwSwapBuffers();
 }
 
 void GFXWindow::quit(int exit_status) {
-	glfwTerminate();
-	exit(exit_status);
+  glfwTerminate();
+  exit(exit_status);
 }
 
--- src/c++/GFX.h
+++ src/c++/GFX.h
@@ -21,41 +21,41 @@
 typedef std::pair<std::vector<GLfloat>, std::vector<GLushort> > GFXObject;
 
 class GFXWindow {
-private:
-	int height, width;
-	float r, g, b, a;
-	std::vector<GFXObject> objects;
-	const char* vshaderPath;
-	const char* fshaderPath;
-	GLuint vao;
+  private:
+    int height, width;
+    float r, g, b, a;
+    std::vector<GFXObject> objects;
+    const char* vshaderPath;
+    const char* fshaderPath;
+    GLuint vao;
 
-	static const int COLOR_DEPTH = 8;
-	static const int ALPHA_DEPTH = 8;
-	static const int DEPTH_BUFFER_BITS = 0;
-	static const int STENCIL_BUFFER_BITS = 0;
-	static const int VERTEX_SIZE = 3;
+    static const int COLOR_DEPTH = 8;
+    static const int ALPHA_DEPTH = 8;
+    static const int DEPTH_BUFFER_BITS = 0;
+    static const int STENCIL_BUFFER_BITS = 0;
+    static const int VERTEX_SIZE = 3;
 
-	// Camera coordinates
-	glm::vec3 eyepoint;
-	glm::vec3 lookat;
-	glm::vec3 up;
+    // Camera coordinates
+    glm::vec3 eyepoint;
+    glm::vec3 lookat;
+    glm::vec3 up;
 
-	glm::mat4 mvp;
-	GLint uniform_mvp;
+    glm::mat4 mvp;
+    GLint uniform_mvp;
 
-public:
-	GFXWindow(int h, int w);
-	void setBG(float r, float g, float b, float a);
-	void setVertexShader(const char* path);
-	void setFragmentShader(const char* path);
-	void setCamera(glm::vec3 eyepoint, glm::vec3 lookat, glm::vec3 up);
-	void addObject(GFXObject obj);
-	GFXObject getObject(int ind);
-	void init();
-	void idle(glm::mat4 model_transform);
-	void render();
-	void createWindow();
-	void quit(int exit_status);
+  public:
+    GFXWindow(int h, int w);
+    void setBG(float r, float g, float b, float a);
+    void setVertexShader(const char* path);
+    void setFragmentShader(const char* path);
+    void setCamera(glm::vec3 eyepoint, glm::vec3 lookat, glm::vec3 up);
+    void addObject(GFXObject obj);
+    GFXObject getObject(int ind);
+    void init();
+    void idle(glm::mat4 model_transform);
+    void render();
+    void createWindow();
+    void quit(int exit_status);
 };
 
 #endif
